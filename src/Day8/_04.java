bc36fed9a92e3582964a47225be6932c469144a3
JGwanghou <jogwangho0619@gmail.com>
2023-06-25 20:44:09 +0900

    0625 programmers

---
+++ src/Day8/_04.java
@@ -0,0 +1,24 @@
+package Day8;
+
+import java.util.stream.IntStream;
+
+/*
+    순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+    자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+ */
+public class _04 {
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++)
+            if(n %i == 0) answer += 1;
+
+        return answer;
+
+//      return (int)IntStream.rangeClosed(1, n).filter(i -> n %i == 0).count();
+    }
+
+    public static void main(String[] args) {
+       solution(20);
+    }
+}
